b7ce1fbe0ee20cab4506d55f719252804e18fb14
Aayush Jain <imjainaayush@gmail.com>
2021-05-19 20:20:16 +0530

    feat: added copy to clipboard

--- index.js
+++ index.js
@@ -2,20 +2,25 @@ function getColorCode() {
     let hex = "012345678abcdef";
     let clr = "";
     for (let i = 0; i < 6; ++i) {
-        let ch = (Math.floor(Math.random() * 16));
-        clr += hex.substring(ch, ch+1);
+        let ch = Math.floor(Math.random() * 16);
+        clr += hex.substring(ch, ch + 1);
     }
     return "#" + clr;
 }
 
+console.log(window.Clipboard);
 for (let i = 0; i < 50; ++i) {
-    const color = document.createElement('div');
-    color.classList.add('color');
-    document.querySelector('.colors-container').appendChild(color);
+    const color = document.createElement("div");
+    color.classList.add("color");
+    document.querySelector(".colors-container").appendChild(color);
+    color.addEventListener("click", (event) => {
+        copyToClipboard(color.innerHTML);
+        alert("Copied the text: " + color.innerHTML);
+    });
 }
 
 function genNewColors() {
-    const colors = document.querySelectorAll('.color');
+    const colors = document.querySelectorAll(".color");
     colors.forEach((x) => {
         var colorCode = getColorCode();
         x.style.backgroundColor = colorCode;
@@ -24,3 +29,12 @@ function genNewColors() {
 }
 
 genNewColors();
+
+function copyToClipboard(text) {
+    const elem = document.createElement("textarea");
+    elem.value = text;
+    document.body.appendChild(elem);
+    elem.select();
+    document.execCommand("copy");
+    document.body.removeChild(elem);
+}
